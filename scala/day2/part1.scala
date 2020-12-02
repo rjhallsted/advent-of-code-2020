ca5c6ca3ec7cfff2ddb93125b8ba66e8fc32b991
rjhallsted <rjhallsted@gmail.com>
2020-12-02 00:21:05 -0800

    Make day2,part1 in scala more functional

--- scala/day2/part1.scala
+++ scala/day2/part1.scala
@@ -1,7 +1,17 @@
+package com.rjhallsted.adventOfCode2020.day2
+
 import scala.io.Source
+import scala.util.Try
 
 object Part1 {
-    def getInputs(filepath: String): List[String] =  Source.fromFile(filepath).getLines().toList
+    def sequence[T](in: List[Option[T]]): Option[List[T]] = if (in contains None) None else Some(in.flatten)
+
+    def getInputs(filepath: String): Option[List[String]] = try {
+        val lines = Source.fromFile(filepath).getLines().toList
+        Some(lines) 
+    } catch {
+        case e: Exception => None
+    }
 
     final case class PolicyPair(
         password: String,
@@ -10,24 +20,31 @@ object Part1 {
         char: Char
     )
 
-    def consumeUntil(line: String, char: Char, index: Int): (String, Int) = {
-        def inner(index: Int, current: String): (String, Int) = if (line(index) == char) (current, index) else inner(index + 1, current + line(index))
+    def consumeUntil(line: String, char: Char, index: Int): Option[(String, Int)] = {
+
+        def inner(index: Int, current: String): Option[(String, Int)] = {
+            if (index == line.length) {
+                return None
+            } else if (line(index) == char) {
+                return Some((current, index))
+            } else {
+                return inner(index + 1, current + line(index))
+            }
+        }
 
         inner(index, "")
     }
 
-    def lineToPolicyPair(line: String): PolicyPair = {
-        val (p1, index1) = consumeUntil(line, '-', 0)
-        val (p2, index2) = consumeUntil(line, ' ', index1 + 1)
-        val (char, index3) = consumeUntil(line, ':', index2 + 1)
-
-        PolicyPair(
+    def lineToPolicyPair(line: String): Option[PolicyPair] = for {
+        (p1, index1) <- consumeUntil(line, '-', 0)
+        (p2, index2) <- consumeUntil(line, ' ', index1 + 1)
+        (char, index3) <- consumeUntil(line, ':', index2 + 1)
+    } yield PolicyPair(
             password = line.substring(index3 + 1),
             p1 = p1.toInt,
             p2 = p2.toInt,
             char = char.charAt(0)
         )
-    }
 
     def countValidPairs(pairs: List[PolicyPair], isValid: (PolicyPair) => Boolean): Int = pairs.map(isValid(_)).count(_ == true)
 
@@ -35,16 +52,20 @@ object Part1 {
         val count = pair.password.count(_ == pair.char)
         count >= pair.p1 && count <= pair.p2
     }
+
+    def getCount(isValid: (PolicyPair) => Boolean): Option[Int] = {
+        val file = "../../inputs/day2.txt"
+        for {
+            inputs <- Part1.getInputs(file)
+            pairs <- sequence(inputs.map(lineToPolicyPair).toList)
+        } yield countValidPairs(pairs, isValid)
+    }
 }
 
 object Main1 extends App {
-    val file = "../../inputs/day2.txt"
-    val inputs = Part1.getInputs(file)
-    
-    val count = Part1.countValidPairs(
-        inputs.map(Part1.lineToPolicyPair).toList,
-        Part1.isValid
-    )
-
-    println(count)
+    val result: String = (Part1.getCount(Part1.isValid)) match {
+        case Some(res) => res.toString
+        case None => "Something broke"
+    }
+    println(result)
 }
