8b7ed2bb26440cd2bf6e5babc30e924a08aa7398
rjhallsted <rjhallsted@gmail.com>
2020-12-01 20:54:49 -0800

    Redo part1 with two-pointer solution

---
+++ python/day1/part1.py
@@ -0,0 +1,35 @@
+import functools
+
+def getInput(filename):
+    f = open(filename)
+    numbers = f.readlines()
+    f.close()
+    return list(map(lambda x: int(x), numbers))
+
+def find2NumbersThatSumTo(goal, numbers, start):
+    numbers = sorted(numbers)
+    end = len(numbers) - 1
+    sum = numbers[start] + numbers[end]
+    while sum != goal and end > start:
+        if sum < goal:
+            start += 1
+        else:
+            end -= 1
+        sum = numbers[start] + numbers[end]
+    if sum == goal:
+        return (numbers[start], numbers[end])
+    else:
+        return False
+
+def main():
+    filename = "input.txt"
+    numbers = getInput(filename)
+    result = find2NumbersThatSumTo(2020, numbers, 0)
+    if (result == False):
+        print("No result")
+    else:
+        print(*result)
+        print(functools.reduce(lambda a,b: a*b, result))
+
+main()
+
