de397069f3f5582cd3710526a56fbea0d6262c1f
rjhallsted <rjhallsted@gmail.com>
2020-12-03 20:38:01 -0800

    Slight cleanup

--- scala/day3/part2.scala
+++ scala/day3/part2.scala
@@ -6,10 +6,7 @@ object Main2 extends App {
     val file = "../../inputs/day3.txt"
     val lines = getInputs(file)
     val slopes = List(Slope(1,1), Slope(3,1), Slope(5,1), Slope(7,1), Slope(1,2))
-    println(slopes)
 
-    val totals = slopes.map(countTrees(lines.tail, _))
-    println(totals)
-    val result = totals.reduce(_ * _)
+    val result = slopes.map(countTrees(lines.tail, _)).reduce(_ * _)
     println(result)
 }
