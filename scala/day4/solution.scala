465597292ef526dca127e431c13fb542361acc08
rjhallsted <rjhallsted@gmail.com>
2020-12-05 13:31:42 -0800

    sovled day 4, part 2 with scala

--- scala/day4/solution.scala
+++ scala/day4/solution.scala
@@ -25,6 +25,54 @@ object Solution {
         cid: Option[String]
     )
 
+    type Rule = Passport => Boolean
+
+    private def stringWithIntRange(low: Int, high: Int, value: String): Boolean = value.toInt >= low && value.toInt <= high
+
+    private def isValidHeight(h: String): Boolean = {
+        val label = h.substring(h.length - 2)
+        val value = h.substring(0, h.length - 2)
+
+        (label, value) match {
+            case ("cm", v) if stringWithIntRange(150, 193, v) => true
+            case ("in", v) if stringWithIntRange(59, 76, v) => true
+            case _ => false
+        }
+    }
+
+    private def isAllHex(s: String): Boolean =
+        s.toCharArray.filterNot(
+            (c: Char) => (c >= '0' && c <= '9')
+                    || (c >= 'a' && c <= 'f')
+        ).length == 0
+
+    private def isAllDigits(s: String): Boolean =
+        s.toCharArray.filterNot(
+            (c: Char) => c >= '0' && c <= '9'
+        ).length == 0
+
+    private def isValidHairColor(h: String): Boolean = 
+        h.length == 7 && h.charAt(0) == '#' && isAllHex(h.substring(1))
+
+    val validEyeColors: List[String] = List(
+        "amb", "blu", "brn", "gry", "grn", "hzl", "oth"
+    )
+
+    val rules: List[Rule] = List(
+        (p: Passport) => stringWithIntRange(1920, 2002, p.byr),
+        (p: Passport) => stringWithIntRange(2010, 2020, p.iyr),
+        (p: Passport) => stringWithIntRange(2020, 2030, p.eyr),
+        (p: Passport) => isValidHeight(p.hgt),
+        (p: Passport) => isValidHairColor(p.hcl),
+        (p: Passport) => validEyeColors.contains(p.ecl),
+        (p: Passport) => p.pid.length == 9 && isAllDigits(p.pid)
+    )
+
+    def isPassportValid(p: Passport, rules: List[Rule]): Boolean = 
+        !(rules.map(_(p)).contains(false))
+
+
+
     def unvalidatedToPassport(p: Unvalidated): Option[Passport] =
         for {
             byr <- p.byr
@@ -56,7 +104,7 @@ object Solution {
         )
     }
 
-    def countFullPassports(input: String): Int =
+    def getFullPassports(input: String): List[Passport] =
         input  // TODO: compose the transformations together.
             .split("\n\n")
             .toList
@@ -64,6 +112,10 @@ object Solution {
             .map(propsToUnvalidated(_))
             .map(unvalidatedToPassport(_))
             .flatten
+
+    def countValidPassports(input: String): Int =
+        getFullPassports(input)
+            .filter(isPassportValid(_, rules))
             .length
 }
 
@@ -79,5 +131,6 @@ object Util {
 object Main extends App {
     val file = "../../inputs/day4.txt"
     val input = Util.getInput(file)
-    println(Solution.countFullPassports(input))
+    println(Solution.getFullPassports(input).length)
+    println(Solution.countValidPassports(input))
 }
